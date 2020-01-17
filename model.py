da24c9414200fdd3f38058426656c25af58c3a2b
venkatesh bellale <bellale15dm@gmail.com>
2020-01-17 23:52:03 +0530

    added session table to support flask session

--- model.py
+++ model.py
@@ -191,3 +191,18 @@ class Verify(db.Model):
     def __init__(self, phone_num, otp):
         self.phone_num = phone_num
         self.otp = otp
+
+# flask session table
+class Sessions(db.Model):
+
+    __tablename__ = "sessions"
+
+    id = db.Column(db.Integer, primary_key=True)
+    session_id = db.Column(db.Integer)
+    data = db.Column(db.String())
+    expiry = db.Column(db.String())
+
+    def __init__(self,session_id,data,expiry):
+        self.session_id = session_id
+        self.data = data
+        self.expiry = expiry
