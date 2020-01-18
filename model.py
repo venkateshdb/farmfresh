e88df4947b62d83255619e79d9973a4fbb827e77
venkatesh bellale <bellale15dm@gmail.com>
2020-01-18 07:54:18 +0530

    added unicode to data column in session table

--- model.py
+++ model.py
@@ -198,8 +198,8 @@ class Sessions(db.Model):
     __tablename__ = "sessions"
 
     id = db.Column(db.Integer, primary_key=True)
-    session_id = db.Column(db.String())
-    data = db.Column(db.String())
+    session_id = db.Column(db.String(collation='utf8'))
+    data = db.Column(db.String(collation='utf8'))
     expiry = db.Column(db.String())
 
     def __init__(self,session_id,data,expiry):
